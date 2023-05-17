a8cbfe7a2a32a9a13cab71148fc81264321a21b0
EkaterinaSoldunova <120333603+EkaterinaSoldunova@users.noreply.github.com>
2023-05-17 19:45:34 +0300

    Add files via upload

---
+++ sem4_pr11_SpringBoot/src/main/java/com/example/sem4_pr11_SpringBoot/Sem4Pr11SpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.sem4_pr11_SpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sem4Pr11SpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sem4Pr11SpringBootApplication.class, args);
+	}
+
+}
---
+++ sem4_pr11_SpringBoot/src/test/java/com/example/sem4_pr11_SpringBoot/Sem4Pr11SpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sem4_pr11_SpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sem4Pr11SpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
